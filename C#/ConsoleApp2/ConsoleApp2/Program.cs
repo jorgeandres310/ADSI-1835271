001408751560f6435f50fca9a1c689e737d14565
jorge andres <jorgeandresloaizagomez@gmail.com>
2019-08-15 20:42:39 -0500

    posicionamiento

---
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese el numero para la posicion :" + (i + 1));
+                num[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("el numero para la posicion :" + (i + 1)+" es : "+ num[i]);
+               
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+
+            for (int i = 0; i < 10; i++) { 
+            {
+                Console.WriteLine("ingrese los numeros");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for(int j = 0; j < 10; j++)
+            {
+                Console.WriteLine("numero en posicion " + num[i]);
+            }
+           
+        }
+    }
+
